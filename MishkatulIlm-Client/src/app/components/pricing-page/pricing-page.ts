21fcb504c877a4c2e39c09b5af462c5295c20f64
hkayy33 <hkayy33@gmail.com>
2026-06-14 15:57:18 +0100

    Feat: pricing page

--- MishkatulIlm-Client/src/app/components/pricing-page/pricing-page.ts
+++ MishkatulIlm-Client/src/app/components/pricing-page/pricing-page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface PricingPlan {
+  hoursPerWeek: number;
+  monthlyPrice: number;
+}
+
 @Component({
   selector: 'app-pricing-page',
   imports: [],
@@ -7,5 +12,12 @@ import { Component } from '@angular/core';
   styleUrl: './pricing-page.scss',
 })
 export class PricingPage {
-
+  readonly plans: PricingPlan[] = [
+    { hoursPerWeek: 1, monthlyPrice: 20 },
+    { hoursPerWeek: 2, monthlyPrice: 40 },
+    { hoursPerWeek: 3, monthlyPrice: 60 },
+    { hoursPerWeek: 4, monthlyPrice: 80 },
+    { hoursPerWeek: 5, monthlyPrice: 100 },
+    { hoursPerWeek: 6, monthlyPrice: 120 },
+  ];
 }
